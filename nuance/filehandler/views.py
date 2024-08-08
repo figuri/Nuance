89b9440191c59f8270f5c829c4c1661a93514f8c
unknown <samtisme@gmail.com>
2024-08-08 11:15:01 -0700

    django added to project and initialized github repo for version control

---
+++ nuance/filehandler/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import UploadedFile
+from django.core.files.storage import FileSystemStorage
+
+# Create your views here.
+#Upload file function to handle- you guessed it- file uploads
+#
